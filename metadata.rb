db715f354fa6a42647c9209f2f28a2f39e38c5c6
m.benchchaoui@cloudbau.de <m.benchchaoui@cloudbau.de>
2013-11-06 12:11:15 +0100

    Change cookbook version

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-maintainer        "Opscode, Inc."
+maintainer        "Cloudbau GmbH"
 license           "Apache 2.0"
 description       "Installs and configures the Glance Image Registry and Delivery Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "5.0.1"
+version           "6.0.0"
 recipe            "glance::api", "Installs packages required for a glance api server"
 recipe            "glance::registry", "Installs packages required for a glance registry server"
 
